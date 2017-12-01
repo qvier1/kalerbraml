0d431f1d349d2bd2041a65b68ea1a400fb8232b3
Bram <q.vier23@gmail.com>
2017-12-01 11:47:05 +0700

    Add files via upload

---
+++ kalerbra.ml/progress/script.js
@@ -0,0 +1,70 @@
+//jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+$(".next").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	next_fs = $(this).parent().next();
+	
+	//ke next step di progressbar pake index next_fs
+	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+	
+	//fieldset
+	next_fs.show(); 
+	//sembunyiin fieldset
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			scale = 1 - (1 - now) * 0.2;
+			left = (now * 50)+"%";
+			opacity = 1 - now;
+			current_fs.css({'transform': 'scale('+scale+')'});
+			next_fs.css({'left': left, 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//custom plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+$(".previous").click(function(){
+	if(animating) return false;
+	animating = true;
+	
+	current_fs = $(this).parent();
+	previous_fs = $(this).parent().prev();
+	
+	//balik ke progress sebelum
+	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+	
+	//nunjukin progress sebelum
+	previous_fs.show(); 
+	//sembunyiin fieldset
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			scale = 0.8 + (1 - now) * 0.2;
+			left = ((1-now) * 50)+"%";
+			opacity = 1 - now;
+			current_fs.css({'left': left});
+			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+		}, 
+		duration: 800, 
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		}, 
+		//custom plugin
+		easing: 'easeInOutBack'
+	});
+});
+function submit() {
+    var myWindow = window.open("", "MsgWindow", "width=200,height=100");
+    myWindow.document.write("<p>Submited! you can now track your scholarships at the progress tab</p>");
+}
